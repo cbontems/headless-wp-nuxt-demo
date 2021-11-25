96e33daf157824f19829beebb951da419a1f848a
cbontems <67456035+cbontems@users.noreply.github.com>
2021-11-25 15:15:13 +0100

    V 1.0

---
+++ pages/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">{{ post.title }}</h1>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <article class="py-5" v-html="post.content"></article>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    async asyncData({ store, params }) {
+        await store.dispatch('posts/getPost', params.slug)
+    },
+    computed: {
+        post() {
+            return this.$store.state.posts.post
+        },
+    },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,53 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <div>
+      <h1 class="text-center">Headless WordPress Nuxt Démo</h1>
+      <div class="container py-4">
+        <div class="row">
+          <post-card v-for="post in posts" :key="post.slug" :post="post" />
+          <div class="d-flex w-100">
+            <div v-if="pageInfo.hasNextPage" class="ml-auto">
+              <NuxtLink :to="{ query: { after: pageInfo.endCursor } }">
+                <span>Next Page</span>
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { PostsState } from '~/store/posts'
 
-export default Vue.extend({})
+const pageCount = 5
+
+export default Vue.extend({
+  async asyncData({ store, query }) {
+    await store.dispatch('posts/getPosts', {
+      after: query.after,
+      before: query.before,
+      first: query.before ? undefined : pageCount,
+      last: query.before ? pageCount : undefined,
+    })
+  },
+  computed: {
+    posts() {
+      return (this.$store.state.posts as PostsState).nodes
+    },
+    pageInfo() {
+      return (this.$store.state.posts as PostsState).pageInfo
+    },
+  },
+  watch: {
+    async $route() {
+      await this.$nuxt.refresh()
+      window.scrollTo(0, 0)
+    },
+  },
+})
 </script>
+
+<style></style>
